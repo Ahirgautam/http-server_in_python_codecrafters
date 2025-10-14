a5ca6225683f0670ffc78aca13f70bd349a901dd
ahirgautam <mehurvala@gmail.com>
2025-10-14 18:04:59 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -6,9 +6,21 @@ def main():
     print("Logs from your program will appear here!")
 
     
-    server_socket = socket.create_server(("localhost", 4221))
-    server_socket.accept() 
+    server_socket = socket.create_server(("localhost", 5000))
+    print("listening to port 5000")
 
+    while True:
+        
+        conn, addr = server_socket.accept()
+        print("Received connection ", addr)
+
+        data = conn.recv(1024).decode("utf-8")
+        print("Request received : ")
+        print(data)
+
+        response = ("HTTP/1.1 200 OK \r\n")
+        conn.sendall(response.encode())
+        conn.close()
 
 if __name__ == "__main__":
     main()
